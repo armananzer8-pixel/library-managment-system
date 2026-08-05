0a04ea67c482e6bbae77a1be73107b30037b4933
arman <armananzer8@gmail.com>
2026-08-06 02:07:35 +0530

    Added Library Management System

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello")
+class Book:
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.available = True
+
+    def display(self):
+        status = "Available" if self.available else "Borrowed"
+        print(f"{self.title} by {self.author} - {status}")
